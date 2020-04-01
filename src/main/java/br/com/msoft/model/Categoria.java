774078015c52ecf8dce591c45bdf0b2adeed36c3
Tiago Oliveira <ti.oliveira2007@gmail.com>
2020-04-01 01:09:40 -0300

    Classe DAO Criada e Testada para Categoria

---
+++ src/main/java/br/com/msoft/model/Categoria.java
@@ -0,0 +1,53 @@
+package br.com.msoft.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
+private static final long serialVersionUID = 1L;
+	
+	public Categoria() {}
+
+	public Categoria(Integer _id, String _cat ) {
+		this.id = _id;
+		this.categoria = _cat;		
+	}
+	
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String categoria;
+	
+}
